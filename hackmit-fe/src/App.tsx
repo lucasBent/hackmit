e7636c86debdcd0f62484204fecb117785ec6c76
Sebastian Lopez <sebastianlopez@Sebastians-MacBook-Pro.local>
2024-09-14 15:55:25 -0400

    Barebones Word Practice View

--- hackmit-fe/src/App.tsx
+++ hackmit-fe/src/App.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import WordPractice from './pages/WordPractice';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,9 +43,13 @@ const App: React.FC = () => (
         <Route exact path="/home">
           <Home />
         </Route>
+        <Route exact path="/word-practice">
+          <WordPractice />
+        </Route>
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+       
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
