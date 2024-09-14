7903ff9418802df14ac9421e5b6b87ea82b76206
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-09-14 13:55:04 -0400

    convex sample set up

---
+++ hackmit-be/convex/words.js
@@ -0,0 +1,8 @@
+import { query } from './_generated/server'
+
+export const getWords = query({
+    args: {},
+    handler: async (ctx) => {
+        return await ctx.db.query('words').collect()
+    },
+})
