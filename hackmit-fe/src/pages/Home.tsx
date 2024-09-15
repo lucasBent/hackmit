ecea54d38f695cf076f8490d33b77c1c31a76d67
Lucas Bent <lucasowenbent@gmail.com>
2024-09-15 05:39:45 -0400

    home stuff

--- hackmit-fe/src/pages/Home.tsx
+++ hackmit-fe/src/pages/Home.tsx
@@ -1,52 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonButton } from '@ionic/react';
-import '../theme/variables.css';
-import './Home.css';
-import Toolbar from '../components/Toolbar.js';
+import { IonContent, IonHeader, IonPage, IonTitle, IonButton, IonLabel, IonCard } from '@ionic/react'
+import '../theme/variables.css'
+import './Home.css'
+import Toolbar from '../components/Toolbar.js'
 
 const Home: React.FC = () => {
-  const wordOfTheDay = 'Technology'
-
-  const getGreeting = () => {
-    const hour = new Date().getHours();
-    if (hour < 12) {
-      return 'Good morning!';
-    } else {
-      return 'Good afternoon!';
+    const getGreeting = () => {
+        const hour = new Date().getHours()
+        if (hour < 12) {
+            return 'Good morning!'
+        } else {
+            return 'Good afternoon!'
+        }
     }
-  };
 
-  const getRandomMessage = (): string => {
-    const messages = [
-      "Let's learn something new today.",
-      "Let's polish up our pronunciation.",
-      "Get ready to learn.",
-      "Enjoy exploring Phonify!"
-    ];
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    return messages[randomIndex];
-  };
-  
-  return (
-    <IonPage>
-      <IonHeader>
-        <Toolbar backButton={false}/>
-      </IonHeader >
-      <IonContent fullscreen className="ion-padding">
-        <IonTitle className = "title">{getGreeting()}</IonTitle>
-        <IonTitle className = "small-title">{getRandomMessage()}</IonTitle>
-        <div className="button-container">
-          <IonButton expand="block" className="small-button" style = {{height: '125px', marginBottom: '20px'}} routerLink='/words'>Learn</IonButton>
+    const getRandomMessage = (): string => {
+        const messages = [
+            "Let's learn something new today.",
+            "Let's polish up our pronunciation.",
+            'Get ready to learn.',
+            'Enjoy exploring I:PAL!',
+        ]
+        const randomIndex = Math.floor(Math.random() * messages.length)
+        return messages[randomIndex]
+    }
 
-          <IonButton expand="block" className='word-of-the-day'  style = {{height: '250px',marginBottom: '20px'}} routerLink={`/word-practice/${wordOfTheDay.toLocaleLowerCase()}`}>
-           {'Word of the Day: ' + wordOfTheDay}
-            </IonButton>
-          <IonButton expand="block" className="small-button" style = {{height: '125px', marginBottom: '20px'}}routerLink='/grid' >Pronounce</IonButton>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
+    return (
+        <IonPage className='home'>
+            <IonHeader>
+                <Toolbar backButton={false} />
+            </IonHeader>
+            <IonContent fullscreen className='ion-padding'>
+                <div className='pal-contact-section'>
+                    <img src='src/assets/bunny-ipal.png' style={{ width: 'auto', height: '4rem' }} />
+                    <div className='homepage-text-wrap'>
+                        <IonTitle className='homepage-title'>{getGreeting()}</IonTitle>
+                        <div className='homepage-subtitle'>{getRandomMessage()}</div>
+                    </div>
+                </div>
 
-export default Home;
+                <div className='button-container'>
+                    <div className='home-button'>
+                        <IonCard className='small-button' routerLink='/word-practice/technology'>
+                            <IonTitle>Word of the day</IonTitle>
+                            <div className='home-card-desc'>Technology</div>
+                        </IonCard>
+                    </div>
+                    <div className='home-button'>
+                        <IonCard className='small-button' routerLink='/words'>
+                            <IonTitle>Practice any word</IonTitle>
+                            <div className='home-card-desc'>Dictionary</div>
+                        </IonCard>
+                    </div>
+                    <div className='home-button'>
+                        <IonCard className='small-button' routerLink='/grid'>
+                            <IonTitle>What is this character?</IonTitle>
+                            <div className='home-card-desc'>IPA Reference</div>
+                        </IonCard>
+                    </div>
+                    <div className='home-button'>
+                        <IonCard className='small-button' routerLink='/grid'>
+                            <IonTitle>Generate practice sentences!</IonTitle>
+                            <div className='home-card-desc'>AI Playground</div>
+                        </IonCard>
+                    </div>
+                </div>
+            </IonContent>
+        </IonPage>
+    )
+}
 
-
+export default Home
