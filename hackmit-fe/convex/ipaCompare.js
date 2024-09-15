2ffc2fe1f4015a2ad9794c314d39f83bd1700bbc
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-09-14 20:31:20 -0400

    algorithm working!!!!

--- hackmit-fe/convex/ipaCompare.js
+++ hackmit-fe/convex/ipaCompare.js
@@ -10,47 +10,30 @@ export const compare = action({
         // implementation goes here
         const original = args.original
         const userInput = args.userInput
-        let differingIndices = []
-        let i = 0,
-            j = 0
+        
+        let differingIndices = [];
+        let i = 0
 
-        // Track if we're in a sequence of consecutive differences
-        let inDifferenceSequence = false
-        let firstDifferenceIndex = -1
-
-        // Compare the characters up to the length of the shorter string
-        while (i < original.length && j < userInput.length) {
-            if (original[i] !== userInput[j]) {
-                if (!inDifferenceSequence) {
-                    // Start of a difference sequence
-                    firstDifferenceIndex = j
-                    inDifferenceSequence = true
-                }
-            } else {
-                if (inDifferenceSequence) {
-                    // End of a difference sequence
-                    differingIndices.push(firstDifferenceIndex)
-                    differingIndices.push(j - 1) // Include the last different index
-                    inDifferenceSequence = false
-                }
+        // Compare characters in both strings until the end of the shorter string
+        while (i < original.length && i < userInput.length) {
+            if (original[i] !== userInput[i]) {
+                differingIndices.push(i) // Flag mismatch
             }
             i++
-            j++
         }
 
-        // If there's an ongoing difference sequence at the end of the loop
-        if (inDifferenceSequence) {
-            differingIndices.push(firstDifferenceIndex)
-            differingIndices.push(j - 1)
+        // Flag any remaining characters in userInput if it's longer than original
+        while (i < userInput.length) {
+            differingIndices.push(i) // Flag extra characters in userInput
+            i++
         }
 
-        // If userInput is longer, add the remaining extra characters' indices
-        while (j < userInput.length) {
-            differingIndices.push(j)
-            j++
+        // If original is longer, flag the remaining characters as missing in userInput
+        while (i < original.length) {
+            differingIndices.push(i) // Flag missing characters in userInput
+            i++
         }
 
-        // Optionally log or return the differing indices
         return differingIndices
     },
 })
