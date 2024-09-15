e0ee077487a016048c4f0483d314067dedce035c
Brianna Patten <brianna.patten@ou.edu>
2024-09-15 07:24:15 -0400

    trying to allow char sounds on practice page:

--- hackmit-fe/src/components/Char.tsx
+++ hackmit-fe/src/components/Char.tsx
@@ -1,20 +1,23 @@
+import React from 'react'
 import './Char.css'
 
 interface CharProps {
     char: string
     result?: string
+    audioUrl: string 
 }
 
-const Char: React.FC<CharProps> = ({ char, result }) => {
+const Char: React.FC<CharProps> = ({ char, result, audioUrl }) => {
     let classNames = 'char '
     if (result) classNames += result
 
-    function handleChar() {
-        alert(char)
+    function handleCharClick() {
+       const audio = new Audio(audioUrl)
+        audio.play().catch(error => console.error('Error playing audio:', error))
     }
 
     return (
-        <div className={classNames} onClick={handleChar}>
+        <div className={classNames} onClick={handleCharClick}>
             {char}
         </div>
     )
